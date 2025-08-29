6d43d59346760bec483edee01ce2a2f78e99db38
menesesmatheus <67125578+menesesmatheus@users.noreply.github.com>
2025-08-29 12:28:27 -0300

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,81 @@
+/*
+==================================================================================
+DDL SCRIPT: CREATE BRONZE TABLES
+==================================================================================
+
+==================================================================================
+Script Purpose
+	This script creates tables in the bronze schema, dropping existing tables
+	if they already exists
+	Run this script to re-define the DDL structure of the bronze schema
+==================================================================================
+*/
+
+--Create table crm_cust_info based on the CRM cust_info_base file
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info (
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR (50),
+	cst_lastname NVARCHAR (50),
+	cst_marital_status NVARCHAR (50),
+	cst_gndr NVARCHAR (50),
+	cst_create_date DATE
+);
+
+--Create table crm_prd_info based on the CRM prd_info file
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info (
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
+);
+
+--Create table crm_sales_details based on the CRM sales_details file
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_d INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
+);
+
+-- Create Table erp_cust_az12 based on the ERP CUST_AZ12 file
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12 (
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50)
+);
+
+--Create Table erp_loc_a101 based on the ERP LOC_A101 file
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101 (
+	cid NVARCHAR (50),
+	cntry NVARCHAR (50)
+);
+
+--Create table erp_px_cat_g1v2 based on the ERP PX_CAT_G1V2 file
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50)
+);
+
